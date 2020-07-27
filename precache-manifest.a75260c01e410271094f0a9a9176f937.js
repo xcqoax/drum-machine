ae9712c1002d907d387f39d07a505c722a1a0595
Gonzalo <palmer_85@hotmail.com>
2020-07-26 19:41:18 -0500

    Updates

---
+++ precache-manifest.a75260c01e410271094f0a9a9176f937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47808ded0b24cc8553c158a562342011",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "b34c364b02af0549bc79",
+    "url": "/drum-machine/static/css/main.c911476e.chunk.css"
+  },
+  {
+    "revision": "e1a71807e48262a12db9",
+    "url": "/drum-machine/static/js/2.1bd3121f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.1bd3121f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34c364b02af0549bc79",
+    "url": "/drum-machine/static/js/main.47e0e448.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
